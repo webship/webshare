6749fea218e6b149e91f511b1f6e373b7954dacd
Shaimaa Natshah <shaimaa.natshah11@gmail.com>
2025-10-11 16:08:07 +0300

    [#3550257] ci: Add .gitlab-ci.yml file to enable GitLab CI/CD pipeline

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,22 @@
 const postcssRtlLogicalProperties = require('postcss-rtl-logical-properties');
 const postcssRTLCSS = require('postcss-rtlcss');
-const Mode = require('postcss-rtlcss').Mode;
+const { Mode } = require('postcss-rtlcss');
 
 module.exports = (ctx) => ({
   map: !ctx.env || ctx.env !== 'production' ? { inline: false } : false,
   plugins: [
     require('postcss-custom-properties')({
       preserve: false,
-      importFrom: [
-        'css/base/variables.css'
-      ]
+      importFrom: ['css/base/variables.css'],
     }),
     require('postcss-calc'),
     require('autoprefixer')({
-      cascade: false
+      cascade: false,
     }),
     postcssRtlLogicalProperties(),
     postcssRTLCSS({
       mode: Mode.override,
-      ignorePrefixedRules: false
-    })
-  ]
+      ignorePrefixedRules: false,
+    }),
+  ],
 });
